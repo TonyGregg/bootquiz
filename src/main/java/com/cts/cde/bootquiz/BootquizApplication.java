cae1444f23b589fc2b11f5dd9e472b196ec3db01
Antony G <antony.gregory@shoes.com>
2021-11-07 20:15:18 -0600

    Initial commit

---
+++ src/main/java/com/cts/cde/bootquiz/BootquizApplication.java
@@ -0,0 +1,13 @@
+package com.cts.cde.bootquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootquizApplication.class, args);
+	}
+
+}
